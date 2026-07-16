34d8b780bc0fad3b66be205c8b0bae25ecf412ee
malejo <marcos_alejandrogarciabravo@soy.sena.edu.com>
2026-07-15 21:52:13 -0500

    v0.1_clasesPaqueteModelCompletas_malejo_15072026

--- src/main/java/com/sena/parqueadero/model/Vehiculo.java
+++ src/main/java/com/sena/parqueadero/model/Vehiculo.java
@@ -1,5 +1,35 @@
 package com.sena.parqueadero.model;
 
-public class Vehiculo {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "vehiculos")
+public class Vehiculo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idvehiculo;
+	@Column(unique = true, nullable = false)
+	private String placa;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoVehiculo tipo;
+	private String modelo;
+	private String Marca;
 }
